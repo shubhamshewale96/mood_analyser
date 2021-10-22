bd4546cae6d8b921acfd55c61f1b145f550fda83
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-22 19:16:02 +0530

    add:handel exception

--- lib/src/test/java/mood_analyser/MoodAnalyserTest.java
+++ lib/src/test/java/mood_analyser/MoodAnalyserTest.java
@@ -7,14 +7,20 @@ public class MoodAnalyserTest {
 	// Test case to analyze sad mood of the user
     @Test
     public void givenMessageWhenProperShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String actualResult = MoodAnalyser.analyseMood("i am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in sad mood");
+        String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
     @Test
     public void givenMessageContainsAnyInitShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser();
-        String actualResult = MoodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Any Mood");
+        String actualResult = MoodAnalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
+    @Test
+    public void givenMessage_Null_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Happy", actualResult);
     }
 }
