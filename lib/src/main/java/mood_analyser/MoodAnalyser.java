5cc9eee1bc5017ec408b71c8647ce688dab8eda9
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-22 19:42:33 +0530

    Throws mood analysis Exception

--- lib/src/main/java/mood_analyser/MoodAnalyser.java
+++ lib/src/main/java/mood_analyser/MoodAnalyser.java
@@ -3,31 +3,34 @@ package mood_analyser;
 public class MoodAnalyser {
 	public static String message;
 
-    // Constructor
-    public MoodAnalyser(String message) {
-        MoodAnalyser.message = message;
-        analyseMood();
-    }
+	public MoodAnalyser() {
+	}
 
-    //Getter
-    public String getMessage() {
-        return message;
-    }
+	// Constructor
+	public MoodAnalyser(String message) throws MoodAnalysisException {
+		MoodAnalyser.message = message;
+		analyseMood();
+	}
 
-    //Setter
-    public void setMessage(String message) {
-        MoodAnalyser.message = message;
-    }
+	// Getter
+	public String getMessage() {
+		return message;
+	}
 
-    public static String analyseMood() {
-        try {
-            if (message.toLowerCase().contains("sad")) {
-                return "Sad";
-            } else {
-                return "Happy";
-            }
-        } catch (NullPointerException e) {
-            return "Happy";
-        }
-    }
+	// Setter
+	public void setMessage(String message) {
+		MoodAnalyser.message = message;
+	}
+
+	public static String analyseMood() throws MoodAnalysisException {
+		try {
+			if (message.toLowerCase().contains("sad")) {
+				return "Sad";
+			} else {
+				return "Happy";
+			}
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException("Entered Invalid mood");
+		}
+	}
 }
--- lib/src/test/java/mood_analyser/MoodAnalyserTest.java
+++ lib/src/test/java/mood_analyser/MoodAnalyserTest.java
@@ -5,23 +5,30 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 	// Test case to analyze sad mood of the user
-    @Test
-    public void givenMessageWhenProperShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in sad mood");
+	@Test
+    public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        //moodAnalyser.setMessage("I am in Sad Mood");
         String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
+
     @Test
-    public void givenMessageContainsAnyInitShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Any Mood");
+    public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
         String actualResult = MoodAnalyser.analyseMood();
         Assert.assertEquals("Happy", actualResult);
     }
+
     @Test
-    public void givenMessage_Null_ShouldReturnHappy() {
+    public void givenMessage_Null_ShouldReturnMessage() throws MoodAnalysisException {
         MoodAnalyser moodanalyser = new MoodAnalyser(null);
-        String actualResult = MoodAnalyser.analyseMood();
-        Assert.assertEquals("Happy", actualResult);
+        try {
+            String actualResult = MoodAnalyser.analyseMood();
+            Assert.assertEquals("Entered Invalid Mood ", actualResult);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
 
